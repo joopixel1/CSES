7007a7ad394d28d31ea84e10c069fd2946091210
joopixel1 <107559561+joopixel1@users.noreply.github.com>
2024-07-09 18:38:06 +0000

    weird_algorithm

---
+++ ZEKAN2/introduction/weird_algorithm.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cout << "Starting..." << "\n";
+
+    int n;
+    cin >> n;
+    cout << n;
+    
+    while(n != 1) {
+        if(n&1) n = n*3 + 1;
+        else n /= 2;
+        cout << " " << n;
+    } 
+ 
+    return (0);
+}
